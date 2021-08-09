6b535a7390752f051fdd62513a28152e87df570e
Martin <martinchipoco@gmail.com>
2021-08-08 23:02:42 -0400

    Vistas y front end

--- web/licoresdom/routes/web.php
+++ web/licoresdom/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// HOME
+Route::view("/","home")->name("home");
+Route::view("/conectar","conectar")->name("conectar");
+Route::view("/productos","productos")->name("productos");
+Route::view("/promociones","promociones")->name("promociones");
+Route::view("/carrito","carrito")->name("carrito");
+Route::view("/proveedores","proveedores")->name("proveedores");
+
+
+//ADMIN VIEWS
+
+Route::view("/admin_home","admin_home")->name("admin_home");
+Route::view("/admin_productos","admin_productos")->name("admin_productos");
+Route::view("/admin_usuarios","admin_usuarios")->name("admin_usuarios");
+Route::view("/admin_proveedores","admin_proveedores")->name("admin_proveedores");
+Route::view("/admin_ventas","admin_ventas")->name("admin_ventas");
+
